d2724714ae3a6a5e3ac01d498f3c5a75a7cf0661
Claudiano Lima <claudianopereira047@gmail.com>
2021-09-06 08:44:24 -0300

    01 - Estrutura da aplicação

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Olá Mundo</h1>
+    </>
+  )
+}
