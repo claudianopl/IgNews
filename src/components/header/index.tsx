44afc03ca2b801b4db17980be86325b5c864717b
Claudiano Lima <claudianopereira047@gmail.com>
2021-09-06 09:47:14 -0300

    02 - Componentes e páginas

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Ig.News" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  )
+}
