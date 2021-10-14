76517a2805cd4714f973a92ff94d99d1553c16c3
Claudiano Lima <claudianopereira047@gmail.com>
2021-10-14 08:54:27 -0300

    01 - Trabalhando com CMS - Prismic

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Ig.News" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
+
         </nav>
 
         <SignInButton />
